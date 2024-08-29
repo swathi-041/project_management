90c8c3a8ce9d10452f23118e52cd97f530f248d5
Swathi <22135a0521@gvpce.ac.in>
2024-08-29 22:28:17 +0530

    changes in backend

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,15 @@ const cors = require("cors");
 const mysql = require("mysql2");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors({
-    origin: ['https://project-management-teal.vercel.app',
-        'https://projecttmanagement.netlify.app']
+    origin: [
+        'https://project-management-teal.vercel.app',
+        'https://projecttmanagement.netlify.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const pool = mysql.createPool({
